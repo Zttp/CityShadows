e08d3ce4b3650013e7b95ac3c6bd4057e8351941
Zttp <162319093+Zttp@users.noreply.github.com>
2025-05-23 23:13:54 +0300

    Create state_node.gd

---
+++ state_node.gd
@@ -0,0 +1,18 @@
+class_name StateNode
+extends Node
+
+var state_machine: StateMachine = null
+var bot: Bot = null
+var state: Bot.State
+
+func enter():
+    pass
+
+func exit():
+    pass
+
+func update(delta: float):
+    pass
+
+func physics_update(delta: float):
+    pass
